a16ddf71239e281bef231fef43d1630e36d9b290
leerssej <jens.leerssen@gmail.com>
2018-06-18 12:15:53 -0700

    HoneyMakerBee passing functionality tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,9 +1,17 @@
 var HoneyMakerBee = function() {
+  Grub.call(this);
   this.age = 10;
   this.job = 'make honey';
   this.color = 'yellow';
-  Grub.call(this, 'food');
+  this.honeyPot = 0;
 };
 
 HoneyMakerBee.prototype = Object.create(Bee.prototype);
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot += 1;
+}
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot -= 1;
+}
