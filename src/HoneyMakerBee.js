8b7bb148cdeb00fdbd41e9430a7184fd7dd0c94e
leerssej <jens.leerssen@gmail.com>
2018-06-18 12:06:33 -0700

    HoneyMakerBee passes tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,9 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = 'yellow';
+  Grub.call(this, 'food');
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
