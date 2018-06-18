a5d2d52150f10a9106dc27119c3479b76a5a4e4a
leerssej <jens.leerssen@gmail.com>
2018-06-18 12:07:42 -0700

    ForagerBee passes tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,8 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "find pollen"
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
